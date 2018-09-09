bd7f756075dbe27c5f72f893e1caa2b7b1582bcd
hauphuong <luonghau91@gmail.com>
2018-09-09 21:59:34 +0700

    Commit Area Admin

--- StdCoreApp/Startup.cs
+++ StdCoreApp/Startup.cs
@@ -97,6 +97,10 @@ namespace StdCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
