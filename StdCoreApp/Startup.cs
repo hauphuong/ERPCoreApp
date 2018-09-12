d59ffbbce0b60465a3c418d723b45483471ad0f9
hauphuong <luonghau91@gmail.com>
2018-09-12 22:24:16 +0700

    Commit login page

--- StdCoreApp/Startup.cs
+++ StdCoreApp/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 using StdCoreApp.Application.Implementations;
 using StdCoreApp.Application.Interfaces;
 using StdCoreApp.Data.EF;
@@ -71,12 +73,13 @@ namespace StdCoreApp
             services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
             services.AddTransient<IProductCategoryService, ProductCategoryService>();
 
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(options=>options.SerializerSettings.ContractResolver = new DefaultContractResolver());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile("Logs/std-{Date}.txt");
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
---
+++ StdCoreApp/wwwroot/app/controllers/login/index.js
@@ -0,0 +1,34 @@
+﻿var loginController = function () {
+    this.initialize = function () {
+        registerEvents();
+    }
+
+    var registerEvents = function () {
+        $('#btnLogin').on('click', function (e) {
+            e.preventDefault();
+            var user = $('#txtUserName').val();
+            var pass = $('#txtPassword').val();
+            login(user, pass);
+        });
+    }
+
+    var login = function (user, pass) {
+        $.ajax({
+            type: 'POST',
+            data: {
+                UserName: user,
+                Password: pass
+            },
+            dataType: 'json',
+            url: '/admin/login/authen',
+            success: function (res) {
+                if (res.Success) {
+                    window.location.href = "/Admin/Home/Index";
+                }
+                else {
+                    std.notify('Đăng nhập không đúng', 'error');
+                }
+            }
+        });
+    }
+}
