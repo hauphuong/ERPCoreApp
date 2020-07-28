b3e30c1c43b4596a64a22ba6cfd6ef797b4197c8
hauphuong <luonghau91@gmail.com>
2020-07-28 16:39:05 +0700

    Commit  up to 44

---
+++ StdCoreApp/Controllers/Components/MobileMenuViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace StdCoreApp.Controllers.Components
+{
+    public class MobileMenuViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
