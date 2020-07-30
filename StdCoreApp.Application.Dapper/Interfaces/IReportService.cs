1c1ffb725d01497082f745a30012ff7ec6bf4831
haulv <luonghau91@gmail.com>
2020-07-30 21:27:34 +0700

    Commit 30/07/2020

---
+++ StdCoreApp.Application.Dapper/Interfaces/IReportService.cs
@@ -0,0 +1,11 @@
+﻿using StdCoreApp.Application.Dapper.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StdCoreApp.Application.Dapper.Interfaces
+{
+    public interface IReportService
+    {
+        Task<IEnumerable<RevenueReportViewModel>> GetReportAsync(string fromDate, string toDate);
+    }
+}
