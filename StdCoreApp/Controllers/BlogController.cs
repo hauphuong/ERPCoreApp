4b8e5f83a7214ae4ef76b1c0ef2111345f51a2de
linhbdl <buidieulinh.bdl@gmail.com>
2020-08-05 16:03:56 +0700

    Commit

---
+++ StdCoreApp/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StdCoreApp.Application.Interfaces;
+
+namespace StdCoreApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private IBlogService _blogService;
+
+        public BlogController(IBlogService BlogService)
+        {
+            _blogService = BlogService;
+        }
+
+        [Route("blog.html", Name = "Blog")]
+        public IActionResult Index(string alias)
+        {
+            var Blog = _blogService.GetByAlias(alias);
+            return View(Blog);
+        }
+    }
+}
---
+++ StdCoreApp/wwwroot/app/controllers/blog/index.js
@@ -0,0 +1,240 @@
+﻿var BlogController = function () {
+    this.initialize = function () {
+        loadData();
+        registerEvents();
+        registerControls();
+    }
+
+    function registerEvents() {
+        //Init validation
+        $('#frmMaintainance').validate({
+            errorClass: 'red',
+            ignore: [],
+            lang: 'en',
+            rules: {
+                txtNameM: { required: true },
+                txtAliasM: { required: true }
+            }
+        });
+
+        $('#txt-search-keyword').keypress(function (e) {
+            if (e.which === 13) {
+                e.preventDefault();
+                loadData();
+            }
+        });
+        $("#btn-search").on('click', function () {
+            loadData();
+        });
+
+        $("#ddl-show-page").on('change', function () {
+            std.configs.pageSize = $(this).val();
+            std.configs.pageIndex = 1;
+            loadData(true);
+        });
+
+        $("#btn-create").on('click', function () {
+            resetFormMaintainance();
+            $('#modal-add-edit').modal('show');
+
+        });
+
+        $('body').on('click', '.btn-edit', function (e) {
+            e.preventDefault();
+            var that = $(this).data('id');
+            $.ajax({
+                type: "GET",
+                url: "/Admin/Blog/GetById",
+                data: { id: that },
+                dataType: "json",
+                beforeSend: function () {
+                    std.startLoading();
+                },
+                success: function (response) {
+                    var data = response;
+                    $('#hidIdM').val(data.Id);
+                    $('#txtNameM').val(data.Name);
+
+                    $('#txtAliasM').val(data.SeoAlias);
+                    CKEDITOR.instances.txtContentM.setData(data.Content);
+                    $('#ckStatusM').prop('checked', data.Status === 1);
+
+                    $('#modal-add-edit').modal('show');
+                    std.stopLoading();
+
+                },
+                error: function () {
+                    std.notify('Có lỗi xảy ra', 'error');
+                    std.stopLoading();
+                }
+            });
+        });
+
+        $('#btnSaveM').on('click', function (e) {
+            if ($('#frmMaintainance').valid()) {
+                e.preventDefault();
+                var id = $('#hidIdM').val();
+                var name = $('#txtNameM').val();
+                var seoAlias = $('#txtAliasM').val();
+                var content = CKEDITOR.instances.txtContentM.getData();
+                var status = $('#ckStatusM').prop('checked') === true ? 1 : 0;
+
+                $.ajax({
+                    type: "POST",
+                    url: "/Admin/Blog/SaveEntity",
+                    data: {
+                        Id: id,
+                        Name: name,
+                        Content: content,
+                        Status: status,
+                        SeoAlias: seoAlias
+                    },
+                    dataType: "json",
+                    beforeSend: function () {
+                        std.startLoading();
+                    },
+                    success: function () {
+                        std.notify('Update blog successful', 'success');
+                        $('#modal-add-edit').modal('hide');
+                        resetFormMaintainance();
+
+                        std.stopLoading();
+                        loadData(true);
+                    },
+                    error: function () {
+                        std.notify('Have an error in progress', 'error');
+                        std.stopLoading();
+                    }
+                });
+                return false;
+            }
+            return false;
+        });
+
+        $('body').on('click', '.btn-delete', function (e) {
+            e.preventDefault();
+            var that = $(this).data('id');
+            std.confirm('Are you sure to delete?', function () {
+                $.ajax({
+                    type: "POST",
+                    url: "/Admin/Blog/Delete",
+                    data: { id: that },
+                    dataType: "json",
+                    beforeSend: function () {
+                        std.startLoading();
+                    },
+                    success: function () {
+                        std.notify('Delete blog successful', 'success');
+                        std.stopLoading();
+                        loadData();
+                    },
+                    error: function () {
+                        std.notify('Have an error in progress', 'error');
+                        std.stopLoading();
+                    }
+                });
+            });
+        });
+    };
+
+    function resetFormMaintainance() {
+        $('#hidIdM').val(0);
+        $('#txtNameM').val('');
+        $('#txtAliasM').val('');
+        CKEDITOR.instances.txtContentM.setData('');
+        $('#ckStatusM').prop('checked', true);
+
+    }
+
+    function registerControls() {
+        var editorConfig = {
+            filebrowserImageUploadUrl: '/Admin/Upload/UploadImageForCKEditor?type=Images'
+        }
+        CKEDITOR.replace('txtContentM', editorConfig);
+        //Fix: cannot click on element ck in modal
+        $.fn.modal.Constructor.prototype.enforceFocus = function () {
+            $(document)
+                .off('focusin.bs.modal') // guard against infinite focus loop
+                .on('focusin.bs.modal', $.proxy(function (e) {
+                    if (
+                        this.$element[0] !== e.target && !this.$element.has(e.target).length
+                        // CKEditor compatibility fix start.
+                        && !$(e.target).closest('.cke_dialog, .cke').length
+                        // CKEditor compatibility fix end.
+                    ) {
+                        this.$element.trigger('focus');
+                    }
+                }, this));
+        };
+    }
+
+    function loadData(isPageChanged) {
+        $.ajax({
+            type: "GET",
+            url: "/admin/blog/GetAllPaging",
+            data: {
+                keyword: $('#txt-search-keyword').val(),
+                page: std.configs.pageIndex,
+                pageSize: std.configs.pageSize
+            },
+            dataType: "json",
+            beforeSend: function () {
+                std.startLoading();
+            },
+            success: function (response) {
+                var template = $('#table-template').html();
+                var render = "";
+                if (response.RowCount > 0) {
+                    $.each(response.Results, function (i, item) {
+                        render += Mustache.render(template, {
+                            Name: item.Name,
+                            SeoAlias: item.SeoAlias,
+                            Id: item.Id,
+                            Status: std.getStatus(item.Status)
+                        });
+                    });
+                    $("#lbl-total-records").text(response.RowCount);
+                    if (render != undefined) {
+                        $('#tbl-content').html(render);
+
+                    }
+                    wrapPaging(response.RowCount, function () {
+                        loadData();
+                    }, isPageChanged);
+
+
+                }
+                else {
+                    $('#tbl-content').html('');
+                }
+                std.stopLoading();
+            },
+            error: function (status) {
+                console.log(status);
+            }
+        });
+    };
+
+    function wrapPaging(recordCount, callBack, changePageSize) {
+        var totalsize = Math.ceil(recordCount / std.configs.pageSize);
+        //Unbind pagination if it existed or click change pagesize
+        if ($('#paginationUL a').length === 0 || changePageSize === true) {
+            $('#paginationUL').empty();
+            $('#paginationUL').removeData("twbs-pagination");
+            $('#paginationUL').unbind("page");
+        }
+        //Bind Pagination Event
+        $('#paginationUL').twbsPagination({
+            totalPages: totalsize,
+            visiblePages: 7,
+            first: 'Đầu',
+            prev: 'Trước',
+            next: 'Tiếp',
+            last: 'Cuối',
+            onPageClick: function (event, p) {
+                std.configs.pageIndex = p;
+                setTimeout(callBack(), 200);
+            }
+        });
+    }
+}
